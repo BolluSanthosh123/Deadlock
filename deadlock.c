cae0c90b358d17ca60215b83606379675c44f249
BolluSanthosh123 <115945191+BolluSanthosh123@users.noreply.github.com>
2026-06-20 12:59:58 +0530

    Update deadlock.c

--- deadlock.c
+++ deadlock.c
@@ -2,40 +2,40 @@
 #include<pthread.h>
 #include<unistd.h>
 
+pthread_mutex_t mutex;
 pthread_mutex_t mutex1;
-pthread_mutex_t mutex2;
 void *thread1(void *arg)
 { 
-    pthread_mutex_lock(&mutex1);
+    pthread_mutex_lock(&mutex);
     printf("thread1 acquire the mutex1\n");
     sleep(5);
     printf("thread1 waiting for mutex2\n");
-    pthread_mutex_lock(&mutex2);
+    pthread_mutex_lock(&mutex1);
     printf("thread1 acquired mutex2\n");
 
-    pthread_mutex_unlock(&mutex2);
     pthread_mutex_unlock(&mutex1);
+    pthread_mutex_unlock(&mutex);
     
     return NULL;
 }
 void *thread2(void *arg)
 {
-    pthread_mutex_lock(&mutex2);
+    pthread_mutex_lock(&mutex1);
     printf("Thread2 acquired mutex2\n");
     sleep(5);
     printf("thread2 waiting for mutex1\n");
-    pthread_mutex_lock(&mutex1);
+    pthread_mutex_lock(&mutex);
     printf("tread2 aqcuired mutex1\n");
 
-    pthread_mutex_unlock(&mutex2);
     pthread_mutex_unlock(&mutex1);
+    pthread_mutex_unlock(&mutex);
     return NULL;
 }
 int main()
 {
 	pthread_t t1,t2;
+	pthread_mutex_init(&mutex,NULL);
 	pthread_mutex_init(&mutex1,NULL);
-	pthread_mutex_init(&mutex2,NULL);
 
 	pthread_create(&t1,NULL,thread1,NULL);
 	pthread_create(&t2,NULL,thread2,NULL);
@@ -43,7 +43,7 @@ int main()
 	pthread_join(t1,NULL);
 	pthread_join(t2,NULL);
 
+	pthread_mutex_destroy(&mutex);
 	pthread_mutex_destroy(&mutex1);
-	pthread_mutex_destroy(&mutex2);
 	return 0;
 }
